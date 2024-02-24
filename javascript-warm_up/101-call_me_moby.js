7b23fe73a4e72af79bce9df5071c0f6d2266dfe5
HofeDivine <hofedive@gmail.com>
2024-02-24 11:48:31 -1200

    commit

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,10 +1,11 @@
-
+// 101-call_me_moby.js
 function callMeMoby(x, theFunction) {
     for (let i = 0; i < x; i++) {
       theFunction();
     }
   }
   
-  module.exports = callMeMoby;
+  module.exports.callMeMoby = callMeMoby;
+  
   
   

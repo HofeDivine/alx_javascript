6973b2ed75b13c6089063d68987f5b273e7c6d08
HofeDivine <hofedive@gmail.com>
2024-02-28 21:16:03 +0000

    commit

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+  constructor(size) {
+    super(size); 
+  }
+
+  charPrint(c) {
+    
+    if (c === undefined) {
+      c = 'X';
+    }
+    
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;

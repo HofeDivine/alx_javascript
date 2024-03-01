8e7b50191f4c5c4f6995fce122a0b1fa2fbb6c33
HofeDivine <hofedive@gmail.com>
2024-03-01 16:06:06 +0000

    commit

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -17,7 +17,7 @@ function saveWebpage(url, filePath) {
 
 const args = process.argv.slice(2);
 if (args.length !== 2) {
-    console.error('Usage: node script.js <URL> <file-path>');
+    
     process.exit(1);
 }
 

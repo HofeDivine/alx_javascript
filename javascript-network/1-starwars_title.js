586e8585502f59ffbcd567f4c6484bf02bc860e0
HofeDivine <hofedive@gmail.com>
2024-03-01 13:18:58 +0000

    commit

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+function getMovieTitle(movieId) {
+    const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+    
+    request.get(url, (error, response, body) => {
+        if (error) {
+            console.error('Error:', error);
+            return;
+        }
+
+        if (response.statusCode !== 200) {
+            console.error('Error:', body);
+            return;
+        }
+
+        const movie = JSON.parse(body);
+        console.log(`Title: ${movie.title}`);
+    });
+}
+
+const args = process.argv.slice(2);
+if (args.length !== 1 || isNaN(args[0])) {
+    console.error('Usage: node script.js <movieId>');
+    process.exit(1);
+}
+
+const movieId = parseInt(args[0]);
+getMovieTitle(movieId);

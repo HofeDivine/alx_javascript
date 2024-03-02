1cf24cc5e68c429e4dbdaf57f7bd121141e13cb0
HofeDivine <hofedive@gmail.com>
2024-03-02 10:55:28 +0000

    commit

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,36 +1,42 @@
 const request = require('request');
 
 function countMoviesWithCharacter(apiUrl, characterId) {
-    request.get(apiUrl, { json: true }, (error, response, body) => {
-        if (error) {
-            console.error('Error:', error);
-            return;
-        }
+    let count = 0;
 
-        if (response.statusCode !== 200) {
-            console.error('Error:', body);
-            return;
-        }
+    function fetchPage(url) {
+        request.get(url, { json: true }, (error, response, body) => {
+            if (error) {
+                console.error('Error:', error);
+                return;
+            }
 
-        let count = 0;
+            if (response.statusCode !== 200) {
+                console.error('Error:', body);
+                return;
+            }
 
+            body.results.forEach(film => {
+                if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+                    count++;
+                }
+            });
 
-        body.results.forEach(film => {
-            
-            if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-                count++;
+            if (body.next) {
+                fetchPage(body.next); 
+            } else {
+                console.log(`Number of movies where Wedge Antilles is present: ${count}`);
             }
         });
+    }
 
-        console.log(`${count}`);
-    });
+    fetchPage(apiUrl);
 }
 
 const apiUrl = process.argv[2];
 if (!apiUrl) {
-
+    console.error('Usage: node script.js <API_URL>');
     process.exit(1);
 }
 
-const characterId = 18; 
+const characterId = 18;
 countMoviesWithCharacter(apiUrl, characterId);
